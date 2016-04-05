944df7afeeb44c05d97438218d795911b2200d33
Aaron Wood <Aaron Wood>
2016-04-05 10:38:18 -0500

    Hud and background music

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class UI : MonoBehaviour {
+	public Text timeT;
+	public Text lapT;
+	private float timeF=0f;
+	private float lapF = 3f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeT.text = string.Format ("Time: {0}",timeF);
+		timeF+=Time.deltaTime;
+		lapT.text = string.Format ("Laps: {0}", lapF);
+	}
+	public void incLap(){
+		lapF -= 1f;
+	}
+}
