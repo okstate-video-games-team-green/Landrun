12a502f28de500aba9d271f310c590b848dec93c
maxshort <maxshort@users.noreply.github.com>
2016-04-14 11:18:34 -0600

    UI working one-way

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuActions : MonoBehaviour {
+
+	public TrackState trackState;
+	public Canvas canvas;
+	public GameObject startPanel;
+	public GameObject learnMorePanel;
+	public GameObject aboutPanel;
+	public GameObject optionsPanel;
+	public GameObject quitPanel;
+
+	public void StartRace() {
+		//hide canvas
+		canvas.enabled= false;
+		startPanel.SetActive (false);
+		trackState.StartRace();
+	}
+
+	public void ShowLearnMore() {
+		HideAllPanels ();
+		learnMorePanel.SetActive (true);
+	}
+
+	public void ShowAbout() {
+		HideAllPanels ();
+		aboutPanel.SetActive (true);
+	}
+
+
+	public void ShowOptions() {
+		HideAllPanels ();
+		optionsPanel.SetActive (true);
+	}
+		
+	public void ShowQuit() {
+		HideAllPanels ();
+		quitPanel.SetActive (true);
+	}
+
+	//used primarily to clear current visible panel
+	private void HideAllPanels() {
+		startPanel.SetActive(false);
+		learnMorePanel.SetActive(false);
+		aboutPanel.SetActive(false);
+		optionsPanel.SetActive(false);
+		quitPanel.SetActive(false);
+	}
+
+}
---
+++ Assets/Scripts/TrackState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TrackState : MonoBehaviour {
+	private bool _inProgress = false;
+	private float _lastRaceStart = 0;
+
+	public bool inProgress {
+		get {
+			return _inProgress;
+		}
+	}
+
+	public float lastRaceStart{
+		get{
+			if (!inProgress) {
+				throw new InvalidOperationException ("No race is in progress so start time does not make sense.");
+			}
+			return _lastRaceStart;
+		}
+	}
+
+	public void StartRace() {
+		_lastRaceStart = Time.time;
+		_inProgress = true;
+	}
+}
