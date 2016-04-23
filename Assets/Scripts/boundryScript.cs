66f25bc5845591a329b0bc3da42c414d931e66ce
Aaron Wood <Aaron Wood>
2016-04-22 20:49:48 -0500

    got track 3 on par with the rest, implemented the cowboy fixed spinout snimation

--- Assets/Scripts/boundryScript.cs
+++ Assets/Scripts/boundryScript.cs
@@ -7,6 +7,8 @@ public class boundryScript : MonoBehaviour {
 	private int lastWaypoint = 0;
 	private bool spinO=false;
 	private int spinC=0;
+	private Vector3 lookPos;
+	private Quaternion rotation;
 
     public GameObject waypointContainer;
 
@@ -30,38 +32,27 @@ public class boundryScript : MonoBehaviour {
                 waypoints[j++] = potentialWaypoints[i];
             }
         }
-
-		// Set the velocity of the player.
-        //GetComponent<Rigidbody>().velocity = Vector3.up * speed;
 	}
-
-//	void Update() {
-//		transform.position = Vector3.MoveTowards (transform.position, waypoints [currentWaypoint].position, speed * Time.deltaTime);
-//	}
-	void FixedUpdate(){
-		if (spinO && spinC < 4) {
-			var lookPos = waypoints[currentWaypoint].position - transform.position;
-			 lookPos.y = 0;
-			 var rotation = Quaternion.LookRotation(lookPos);
-			 rotation *= Quaternion.Euler(0, 90, 0); // this add a 90 degrees Y rotation
-			 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime*50);
+	void FixedUpdate(){//implements the spin out animation 
+		if (spinO && spinC == 0) {
+			lookPos = waypoints[currentWaypoint].position - transform.position;
+			lookPos.y = 0;
+			rotation = Quaternion.LookRotation(lookPos);
 			spinC++;
-			print ("spin out player");
+		}
+		else if (spinO && spinC < 360) {
+			
+			 rotation *= Quaternion.Euler(0, 1, 0); // this add a 1 degree Y rotation
+			 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+			spinC++;
 		} else {
 			spinO = false;
 			spinC = 0;
 		}
 	}
 
-	void Update () {
+	void Update () {//detects if player is out of bounds and places him back on track
 		Vector3 movementVector = NavigateTowardWaypoint();
-
-		// Set velocity to direction * speed * deltaTime.
-		//GetComponent<Rigidbody>().velocity = movementVector.normalized * speed * Time.deltaTime;
-		//GetComponent<Rigidbody>().rotation = Quaternion.Euler((movementVector.normalized) * rotationSpeed * Time.deltaTime);
-		//GetComponent<Rigidbody>().rotation = Quaternion.Euler(0,movementVector.y-30,0);
-		//transform.LookAt(movementVector);
-		//movementVector.y=0;
 		if (movementVector.magnitude >= 1600.0f) {
 			transform.position = waypoints [lastWaypoint].position;
 			transform.rotation = Quaternion.LookRotation(movementVector);
@@ -69,7 +60,7 @@ public class boundryScript : MonoBehaviour {
 
 	}
 
-    Vector3 NavigateTowardWaypoint()
+    Vector3 NavigateTowardWaypoint()//updates current waypoint and last waypoint
     {
 		// This tells us how close we get to a waypoint before we turn our attention
 		// to the next waypoint.
@@ -77,29 +68,20 @@ public class boundryScript : MonoBehaviour {
 
 		Vector3 movementVector = waypoints[currentWaypoint].position - transform.position;
 
-		//print("*** Distance: " + movementVector.magnitude);
-		//print (currentWaypoint);
-
 		// Are we in the neighborhood of the target waypoint?
 		if (movementVector.magnitude <= neighborhood)
         {
 			// Yes; focus our attention on the next waypoint in the path.
 			lastWaypoint=currentWaypoint;
 			currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
-//			int rand = Random.Range(1, waypoints.Length -1);
-//          currentWaypoint = (currentWaypoint + rand) % waypoints.Length;
-
-          // print("Current waypoint: " + currentWaypoint + "    Relative Position: " + movementVector);
         }
 		movementVector = waypoints[lastWaypoint].position - transform.position;
-		//print("*** Distance: " + movementVector.magnitude);
         return movementVector;
     }
-	void OnTriggerEnter(Collider other){
+	void OnTriggerEnter(Collider other){//detects collision with ai
 		Vector3 aiVec = waypoints [currentWaypoint].position - other.transform.position;
 		Vector3 plVec = waypoints [currentWaypoint].position - transform.position;
 		if (other.name.Equals("Cube")&&plVec.magnitude<aiVec.magnitude){
-			//print ("spin out player");
 			spinO=true;
 		}
 	}
